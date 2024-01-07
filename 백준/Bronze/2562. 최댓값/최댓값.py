e33c5626a88eae2a7576b8facdaa73120a14560e
Bogyoi <86239847+sonlucy@users.noreply.github.com>
2024-01-07 13:50:00 +0900

    [Bronze III] Title: 최댓값, Time: 116 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -0,0 +1,7 @@
+# list=[]
+# for _ in range(9):
+#     list.append(int(input()))
+# print(max(list), list.index(max(list))+1 )
+
+list =[int(input()) for _ in range(9)]
+print(max(list), list.index(max(list))+1 )
