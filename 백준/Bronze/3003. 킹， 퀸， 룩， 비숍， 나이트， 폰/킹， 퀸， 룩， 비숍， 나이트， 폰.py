ad35712ff2671b9b401e298d421a289263e3281c
Bogyoi <86239847+sonlucy@users.noreply.github.com>
2024-01-31 01:23:54 +0900

    Update 킹， 퀸， 룩， 비숍， 나이트， 폰.py

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
@@ -1,6 +1,7 @@
 arr1=list(map(int, input().split()))
 arr2=[1,1,2,2,2,8] # 킹, 퀸, 룩, 비숍, 나이트, 폰 -> 1, 1, 2, 2, 2, 8
 
+# 첫번째 방법 #
 result = [str(arr2[i]-arr1[i]) for i in range(6)]
 print(" ".join(result))
 
@@ -9,3 +10,7 @@ print(" ".join(result))
 #for i in range(6):
     #res[i]=arr2[i]-arr1[i]
     #print(res[i],end=' ')
+
+# 세번째 방법 #
+#for i in range(6):
+#  print(arr2[i] - arr1[i], end=' ')
