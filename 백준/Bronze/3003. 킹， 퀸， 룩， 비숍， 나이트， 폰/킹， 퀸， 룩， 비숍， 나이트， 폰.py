d7b1111b8ca03c96c2c71f6dcf9d082414765b20
Bogyoi <86239847+sonlucy@users.noreply.github.com>
2024-01-31 01:16:17 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
@@ -0,0 +1,11 @@
+arr1=list(map(int, input().split()))
+arr2=[1,1,2,2,2,8] # 킹, 퀸, 룩, 비숍, 나이트, 폰 -> 1, 1, 2, 2, 2, 8
+
+result = [str(arr2[i]-arr1[i]) for i in range(6)]
+print(" ".join(result))
+
+# 두번째 방법 #
+#res=[0]*len(arr1)
+#for i in range(6):
+    #res[i]=arr2[i]-arr1[i]
+    #print(res[i],end=' ')
