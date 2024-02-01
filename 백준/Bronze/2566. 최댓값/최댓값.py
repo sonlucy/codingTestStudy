29e249eed3b3302c4c662ab58e7d7f91dee13f0b
Bogyoi <86239847+sonlucy@users.noreply.github.com>
2024-02-01 20:40:53 +0900

    [Bronze III] Title: 최댓값, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.py
+++ 백준/Bronze/2566. 최댓값/최댓값.py
@@ -1,10 +1,15 @@
-arr=[0]*9
-for i in range(9):
-    arr[i]=list(map(int, input().split()))
+table = [list(map(int, input().split())) for _ in range(9)]
 
+# 첫번째 방법 #
+'''
 for i in range(9):
-    for j in range(9):
-        if arr[i][j]==max(map(max, arr)):
-            a,b=i+1,j+1
-
-print(max(map(max, arr)), a,b)
+  for j in range(9):
+    if table[i][j]==max(map(max, table)): # 현재 위치가 테이블에서 가장 큰 값이면
+      row_idx, col_idx = i+1 ,j+1 #현재 위치를 저장
+print(max(map(max, table)), row_idx, col_idx)
+'''
+# 두번째 방법 #
+max_value = max(map(max, table))
+for row, col_list in enumerate(table): # 각 행을 순회
+  if max_value in col_list: # 맥스값이 있으면
+    print(max_value, row + 1, col_list.index(max_value) + 1)
