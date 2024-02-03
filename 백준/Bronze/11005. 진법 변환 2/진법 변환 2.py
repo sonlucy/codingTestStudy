b4876e356f42778198613868f3719f239f20596e
Bogyoi <86239847+sonlucy@users.noreply.github.com>
2024-02-03 17:11:58 +0900

    [Bronze I] Title: 진법 변환 2, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.py
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.py
@@ -6,7 +6,7 @@ num='0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ' #Z:35
 나머지 (%) 넣는데, N=1일때 마지막으로 나머지 계산해서 넣고 끝.
 출력할땐 역순으로 출력
 '''
-
+'''
 result='' #문자열로 저장할거임
 
 while (N>=1):
@@ -14,3 +14,15 @@ while (N>=1):
   N=N//B
 
 print(result[::-1]) #문자열을 역순으로 출력
+'''
+
+# 두번째 방법
+result_list = []
+
+while N >= 1:
+    result_list.append(num[N % B])
+    N //= B
+
+# 리스트를 역순으로 출력
+for char in reversed(result_list):
+    print(char, end='')
