fa0ddc21e7d2f5d705fde08a520444e83e3bab9f
Bogyoi <86239847+sonlucy@users.noreply.github.com>
2024-02-24 22:11:44 +0900

    Update 알고리즘 수업 － 알고리즘의 수행 시간 6.py

--- 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.py
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.py
@@ -1,3 +1,19 @@
+### 첫번째 방법
+'''
+N= int(input())
+N-=2
+i=1
+result=0
+while (N>0):
+  result+=N*i
+  N-=1
+  i+=1
+print(result)
+print('3')
+
+'''
+
+### 두번째 방법
 N= int(input())
 print(N*(N-1)*(N-2)//6)
 print('3')
