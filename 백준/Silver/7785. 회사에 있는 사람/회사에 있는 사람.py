9b89b9fbc7f1e9962533b5f7d1d329ed18411e95
Bogyoi <86239847+sonlucy@users.noreply.github.com>
2024-05-13 22:25:54 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 228 ms, Memory: 120000 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
@@ -1,15 +1,16 @@
 import sys
 
-n=int(sys.stdin.readline())
-arr=[]
+n = int(sys.stdin.readline().strip())
+arr = set()  # 사용자 목록을 집합으로 관리 (중복 방지 및 삽입/삭제 O(1))
 
 for _ in range(n):
-  a,b=sys.stdin.readline().strip().split()
-  if b=='enter':
-    arr.append(a)
-  elif b=='leave':
-    arr.remove(a)
+    a, b = sys.stdin.readline().strip().split()
+    if b == 'enter':
+        arr.add(a)  
+    elif b == 'leave':
+        if a in arr:
+            arr.remove(a)
 
-arr.sort(reverse=True)
-for i in arr:
-  print(i)
+sorted_users = sorted(arr, reverse=True)  
+for user in sorted_users:
+    print(user)
